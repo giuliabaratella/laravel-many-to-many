879d336da8cdbfe3b68fef0a0352aaf123e4b4ef
giuliabaratella <giuliabaratella7@gmail.com>
2024-01-17 11:28:39 +0100

    updated technology and Project models

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -22,6 +22,10 @@ class Project extends Model
     {
         return $this->belongsTo(Category::class);
     }
+    public function technologies()
+    {
+        return $this->belongsToMany(Technology::class);
+    }
     public static function getSlug($title)
     {
         $slug = Str::of($title)->slug('-');
--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -4,9 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
 
 class Technology extends Model
 {
     use HasFactory;
     protected $fillable = ['name', 'slug'];
+
+    public function projects()
+    {
+        return $this->belongsToMany(Project::class);
+    }
+
+    public static function getSlug($name)
+    {
+        $slug = Str::of($name)->slug('-');
+        $count = 1;
+
+        while (Technology::where("slug", $slug)->first()) {
+            $slug = Str::of($name)->slug('-') . "-{$count}";
+            $count++;
+        }
+        return $slug;
+    }
 }
+
